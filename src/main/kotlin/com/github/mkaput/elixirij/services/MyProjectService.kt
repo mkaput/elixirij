829851bc264812d5792cd13e98ea59943fdb94f8
GitHub Action <action@github.com>
2025-11-26 08:53:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/mkaput/elixirij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mkaput/elixirij/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.mkaput.elixirij.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mkaput.elixirij.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
