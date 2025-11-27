4463fb7beadeb14ed85febe1fbd2100f84ec8f9e
Marek Kaput <marek.kaput@outlook.com>
2025-11-27 16:50:26 +0100

    Apply suggestions from code review

--- src/main/kotlin/dev/murek/elixirij/commenter/ExCommenter.kt
+++ src/main/kotlin/dev/murek/elixirij/commenter/ExCommenter.kt
@@ -2,19 +2,10 @@ package dev.murek.elixirij.commenter
 
 import com.intellij.lang.Commenter
 
-/**
- * Commenter for Elixir language.
- *
- * Elixir uses `#` for single-line comments and does not have block comments.
- */
 class ExCommenter : Commenter {
-    override fun getLineCommentPrefix(): String = "# "
-
+    override fun getLineCommentPrefix(): String = "#"
     override fun getBlockCommentPrefix(): String? = null
-
     override fun getBlockCommentSuffix(): String? = null
-
     override fun getCommentedBlockCommentPrefix(): String? = null
-
     override fun getCommentedBlockCommentSuffix(): String? = null
 }
