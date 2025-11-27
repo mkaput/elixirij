472043ec99fb63014a02957d378895c9589a9c01
Marek Kaput <marek.kaput@outlook.com>
2025-11-27 17:15:24 +0100

    simplify IconLoader call in ExIcons FILE definition

--- src/main/kotlin/dev/murek/elixirij/ExIcons.kt
+++ src/main/kotlin/dev/murek/elixirij/ExIcons.kt
@@ -1,12 +1,8 @@
 package dev.murek.elixirij
 
 import com.intellij.openapi.util.IconLoader
-import javax.swing.Icon
 
-/**
- * Icons for Elixir language support.
- */
 object ExIcons {
     @JvmField
-    val FILE = IconLoader.getIcon("/icons/elixir.svg", ExIcons::class.java)
+    val FILE = IconLoader.getIcon("/icons/elixir.svg", javaClass)
 }
