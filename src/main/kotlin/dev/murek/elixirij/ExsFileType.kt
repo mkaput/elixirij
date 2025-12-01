52d04e834d3014dced05662e337cd3222a3bf909
Marek Kaput <marek.kaput@swmansion.com>
2025-12-01 15:15:16 +0100

    fix filetype conflict

--- src/main/kotlin/dev/murek/elixirij/ExsFileType.kt
+++ src/main/kotlin/dev/murek/elixirij/ExsFileType.kt
@@ -3,8 +3,10 @@ package dev.murek.elixirij
 import com.intellij.openapi.fileTypes.LanguageFileType
 import javax.swing.Icon
 
-object ExsFileType : LanguageFileType(ExLanguage) {
+object ExsFileType : LanguageFileType(ExLanguage, true) {
     override fun getName(): String = "Elixir Script"
+
+    override fun getDisplayName(): String = ExBundle.message("filetype.elixirScript.name")
 
     override fun getDescription(): String = ExBundle.message("filetype.elixirScript.description")
 
