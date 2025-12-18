de672c2f6ff9f8e03d715da21bdbb528402425cd
Marek Kaput <marek.kaput@outlook.com>
2025-12-18 19:57:23 +0100

    introduce "None" code intelligence service and refactor expert mode handling

---
+++ src/main/kotlin/dev/murek/elixirij/lsp/NoCodeIntelligenceService.kt
@@ -0,0 +1,14 @@
+package dev.murek.elixirij.lsp
+
+import com.intellij.platform.lsp.api.LspServerSupportProvider
+import java.nio.file.Path
+
+object NoCodeIntelligenceService : ExLspServerService {
+    override fun ensureServerStarted(serverStarter: LspServerSupportProvider.LspServerStarter) {}
+
+    override fun checkReady(): Boolean = false
+
+    override fun currentExecutable(): Path? = null
+
+    override fun checkUpdates() {}
+}
