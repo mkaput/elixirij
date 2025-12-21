6ef383852457da9953e4a095700807f1caa55556
Marek Kaput <marek.kaput@outlook.com>
2025-12-21 22:32:14 +0100

    wipe parser tests

--- src/test/kotlin/dev/murek/elixirij/lang/ExParserTest.kt
+++ src/test/kotlin/dev/murek/elixirij/lang/ExParserTest.kt
@@ -3,18 +3,6 @@ package dev.murek.elixirij.lang
 import com.intellij.testFramework.ParsingTestCase
 
 class ExParserTest : ParsingTestCase("parser", "ex", ExParserDefinition()) {
-    
+
     override fun getTestDataPath(): String = "src/test/testData"
-    
-    fun testSimpleInteger() = doTest(true)
-    
-    fun testSimpleString() = doTest(true)
-    
-    fun testSimpleAtom() = doTest(true)
-    
-    fun testSimpleIdentifier() = doTest(true)
-    
-    fun testSimpleAlias() = doTest(true)
-    
-    fun testMultipleExpressions() = doTest(true)
 }
